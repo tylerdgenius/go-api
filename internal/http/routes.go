edb5bed140b1c64e6efa146ceb2cd079c9f587d7
Chima Nwachukwu <albertnwachukwu@gmail.com>
2025-07-30 20:38:36 +0000

    feat: setup full routing and http server

--- internal/http/routes.go
+++ internal/http/routes.go
@@ -4,10 +4,15 @@ import (
 	"context"
 
 	"github.com/go-chi/chi/v5"
+	"github.com/go-chi/chi/v5/middleware"
 )
 
 func NewRouter(ctx context.Context, config Config) (*chi.Mux, error) {
 	router := chi.NewRouter()
+
+	router.Use(middleware.SetHeader("Content-Type", "application/json"))
+
+	router.Use(middleware.Heartbeat("/ping"))
 
 	return router, nil
 }
